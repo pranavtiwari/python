a6a82b85a74258425392a21ec71a81679912db2f
Pranav Tiwari <pkt@google.com>
2014-08-22 14:16:04 +0530

    adding ngram.py

---
+++ ngrams.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import urllib2, re
+from collections import Counter
+
+n = 3 #size of n-gram
+
+#You can substitute the link below with any web page with plain text
+url = 'http://www.gutenberg.org/cache/epub/76/pg76.txt'
+
+page = urllib2.urlopen(url)
+
+contents = page.read()
+#contents = contents[:len(contents)/500] #smaller data/less memory option
+
+words = re.findall(r'\w+', contents) #Regular expression to find all words
+
+ngrams = [] #A list to store the ngrams
+i = 0
+
+# while i < len(words):
+#     ngrams.append(tuple(words[i:i+n])) #Splits "words" into n sized tuples
+#     i += n
+
+# print Counter(ngrams) #Calculates the frequency of each n-gram
+
+print Counter(words).most_common(100);
